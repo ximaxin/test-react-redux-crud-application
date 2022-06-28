08ccddabe11059f6c14d70a86240586e9bb83aac
ximaxin <shimazaki@shimashou.com>
2022-06-28 15:54:47 +0900

    create class components and functional components

--- front/react-app/src/App.js
+++ front/react-app/src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-function App() {
+// function App() {
   
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked.")}} />
+//     </React.Fragment>
+//   );
+// }
+
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked.")}} />
-    </React.Fragment>
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  ) 
 }
+
+const Cat = () => {
+  return <div>Meow!!!!!!</div>
+}
+
+
 export default App;
